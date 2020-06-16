71636c69060b8e5b4995bdfd98c8ce139a4eac5f
ramya-itara <ramya@itarait.com>
2020-06-16 17:42:17 +0530

    16062020

---
+++ oi_credit_limit_delivered_qty_override_with_peroid/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Customer Credit Limit - Delivered Quantity(Override)",
+    "version": "12.0.1.0.0",
+    "summary": "",
+    "license": "OPL-1",
+    "depends": ["base","stock","oi_credit_limit_delivered_qty_with_peroid","account"],
+    "author": "Oodu Implementers Private Limited",
+    "website": "https://www.odooimplementers.com",
+    "category": "Account & Stock",
+    "description": "This module is to calculate the credit limit for the customer based on delivered quantity with override in delivery order.",
+    "data": [
+        "security/security.xml",
+        "views/picking.xml",
+    ],
+    "images": ['static/description/banner.gif'],
+    "active": False,
+    "installable": True,
+    
+}
---
+++ payslip_bulk_approval/models/payslip.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class HrPayslipInherit(models.Model):
+	_inherit = 'hr.payslip'
+
+	@api.multi
+	def action_approve(self):
+		for each in self:
+			if each.state == 'verify':
+				each.write({'state': 'done'})
+			else:
+				raise UserError(_("You can't select the Draft records."))
+		return True
