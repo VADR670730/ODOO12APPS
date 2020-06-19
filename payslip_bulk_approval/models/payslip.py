3a12e8753c51ab414a3f87e08f71e741debb00d2
ramya-itara <ramya@itarait.com>
2020-06-19 16:35:21 +0530

    19062020

--- payslip_bulk_approval/models/payslip.py
+++ payslip_bulk_approval/models/payslip.py
@@ -5,11 +5,8 @@ from odoo.exceptions import UserError
 class HrPayslipInherit(models.Model):
 	_inherit = 'hr.payslip'
 
+
 	@api.multi
 	def action_approve(self):
-		for each in self:
-			if each.state == 'verify':
-				each.write({'state': 'done'})
-			else:
-				raise UserError(_("You can't select the Draft records."))
+		self.write({'state': 'done'})
 		return True
